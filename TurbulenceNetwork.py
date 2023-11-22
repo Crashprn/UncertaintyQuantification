514a9ca6344eaf1efcbed018a1db04d642ed1fc3
Cody Grogan <thewall907@gmail.com>
2023-11-22 12:50:59 -0700

    Added Dropout Uncertainty

--- TurbulenceNetwork.py
+++ TurbulenceNetwork.py
@@ -3,18 +3,20 @@ import torch.nn as nn
 import typing as t
 
 class TurbulenceNetwork(nn.Module):
-    def __init__(self, input_dim: int, output_dim: int, num_layers: int, nodes: t.List[int]) -> None:
+    def __init__(self, input_dim: int, output_dim: int, num_layers: int, nodes: t.List[int], dropout=0.0) -> None:
         super(TurbulenceNetwork, self).__init__()
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.layers = num_layers
         self.nodes = nodes
+        self.dropout = dropout
         
         layers = []
         for i in range(num_layers):
             if i == 0:
                 layers.append(nn.Linear(input_dim, nodes[i], dtype=torch.float64))
                 layers.append(nn.ReLU())
+                layers.append(nn.Dropout(self.dropout))
 
             elif i == num_layers - 1:
                 layers.append(nn.Linear(nodes[i-1], output_dim, dtype=torch.float64))
@@ -23,11 +25,18 @@ class TurbulenceNetwork(nn.Module):
             else:
                 layers.append(nn.Linear(nodes[i-1], nodes[i], dtype=torch.float64))
                 layers.append(nn.ReLU())
+                layers.append(nn.Dropout(self.dropout))
 
                 
-        self.model1 = nn.Sequential(*layers)
+        self.model = nn.Sequential(*layers)
+    
+    def dropout_on(self, p):
+        for m in self.model.modules():
+            if isinstance(m, nn.Dropout):
+                m.p = p
+                m.train()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        out1 = self.model1(x)
+        out1 = self.model(x)
         return out1
         
--- plot_utils.py
+++ plot_utils.py
@@ -23,38 +23,26 @@ def plot_data_generation_diff(etas):
 
     plt.show()
 
-def plot_heat_map(model):
-    dim = 1_000
+def plot_heat_map_compare(grid_x, grid_y, target, pred):
     levels = 100
-    x, y = np.meshgrid(np.linspace(-.5, 2, dim),np.linspace(-.5, 2, dim))
-    eta1 = (10**x.flatten())**2
-    eta2 = (10**y.flatten())**2
 
-    gen = TurbulenceClosureDataGenerator(model="SSG", type='torch')
-    etas, G_s = gen(eta1, eta2)
-    G_s = G_s.T.reshape(3, dim, dim)
+    fig, axs = plt.subplots(2, 3, figsize=(15, 10))
 
-    pred = model.predict(etas)
-
-    pred = pred.T.reshape(3, dim, dim)
-
-    fig, axs = plt.subplots(2, 3)
-
-    for (ax0, ax1, ax2), z in zip(axs, [G_s, pred]):
+    for (ax0, ax1, ax2), z in zip(axs, [target, pred]):
         G_1 = z[0]
-        contour_z1 = ax0.contourf(x, y, G_1, levels=levels, cmap='jet')
+        contour_z1 = ax0.contourf(grid_x, grid_y, G_1, levels=levels, cmap='jet')
         ax0.set_title(f"G_1")
         ax0.set_xlabel("log(R)")
         ax0.set_ylabel("log(S)")
 
         G_2 = z[1]
-        contour_z2 = ax1.contourf(x, y, G_2, levels=levels, cmap='jet')
+        contour_z2 = ax1.contourf(grid_x, grid_y, G_2, levels=levels, cmap='jet')
         ax1.set_title(f"G_2")
         ax1.set_xlabel("log(R)")
         ax1.set_ylabel("log(S)")
 
         G_3 = z[2]
-        contour_z3 = ax2.contourf(x, y, G_3, levels=levels, cmap='jet')
+        contour_z3 = ax2.contourf(grid_x, grid_y, G_3, levels=levels, cmap='jet')
         ax2.set_title(f"G_3")
         ax2.set_xlabel("log(R)")
         ax2.set_ylabel("log(S)")
@@ -66,21 +54,25 @@ def plot_heat_map(model):
     fig.suptitle("SSG Algebraic Reynolds Stress Model (Top Algebraic, Bottom Neural Network)")
     plt.show()
 
-    fig, (ax0, ax1, ax2) = plt.subplots(1, 3)
+    
 
-    error = np.abs(G_s - pred)
+def plot_heat_map_loss(x_grid, y_grid, target, pred):
+    fig, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(15, 5))
+    levels = 100
 
-    contour_z1 = ax0.contourf(x, y, error[0], levels=levels, cmap='jet')
+    error = np.abs(target - pred)
+
+    contour_z1 = ax0.contourf(x_grid, y_grid, error[0], levels=levels, cmap='jet')
     ax0.set_title(f"G_1")
     ax0.set_xlabel("log(R)")
     ax0.set_ylabel("log(S)")
 
-    contour_z2 = ax1.contourf(x, y, error[1], levels=levels, cmap='jet')
+    contour_z2 = ax1.contourf(x_grid, y_grid, error[1], levels=levels, cmap='jet')
     ax1.set_title(f"G_2")
     ax1.set_xlabel("log(R)")
     ax1.set_ylabel("log(S)")
 
-    contour_z3 = ax2.contourf(x, y, error[2], levels=levels, cmap='jet')
+    contour_z3 = ax2.contourf(x_grid, y_grid, error[2], levels=levels, cmap='jet')
     ax2.set_title(f"G_3")
     ax2.set_xlabel("log(R)")
     ax2.set_ylabel("log(S)")
