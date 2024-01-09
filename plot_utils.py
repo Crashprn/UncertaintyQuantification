fea4cc2b3f7b028c57f1a7cd19d87fa48fceedd7
Cody Grogan <thewall907@gmail.com>
2024-01-09 14:22:43 -0700

    Added functionality create different regions of data for uncertainty testing

--- plot_utils.py
+++ plot_utils.py
@@ -1,20 +1,23 @@
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from matplotlib import ticker
 from TurbulenceClosureDataGenerator import TurbulenceClosureDataGenerator
 import numpy as np
+import math
 
 def plot_data_generation_diff(etas): 
 
     eta1 = etas[:, 0]
     eta2 = etas[:, 1]
 
-    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
 
     axs[0].scatter(eta1, eta2, marker='.')
-    axs[0].set_xlabel("Log(sqrt(eta1))")
-    axs[0].set_ylabel("Log(sqrt(eta2))")
+    axs[0].set_xlabel("Log(R)")
+    axs[0].set_ylabel("Log(S)")
 
-    eta1 = (10**eta1)**2
-    eta2 = (10**eta2)**2
+    eta1 = (10**eta1)
+    eta2 = (10**eta2)
 
     axs[1].scatter(eta1, eta2, marker='.')
     axs[1].set_title("Linear Scale")
@@ -26,7 +29,7 @@ def plot_data_generation_diff(etas):
 def plot_heat_map_compare(grid_x, grid_y, target, pred):
     levels = 100
 
-    fig, axs = plt.subplots(2, 3, figsize=(15, 10))
+    fig, axs = plt.subplots(2, 3, figsize=(20, 10))
 
     for (ax0, ax1, ax2), z in zip(axs, [target, pred]):
         G_1 = z[0]
@@ -56,11 +59,13 @@ def plot_heat_map_compare(grid_x, grid_y, target, pred):
 
     
 
-def plot_heat_map_loss(x_grid, y_grid, target, pred):
-    fig, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(15, 5))
-    levels = 100
+def plot_heat_map_loss(x_grid, y_grid, target, pred, base=2, levels=20):
+    fig, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(20, 5))
 
     error = np.abs(target - pred)
+    levels_1 = np.logspace(math.log(error[0].min(), base), math.log(error[0].max(), base), levels, base=base)
+    levels_2 = np.logspace(math.log(error[1].min(), base), math.log(error[1].max(), base), levels, base=base)
+    levels_3 = np.logspace(math.log(error[2].min(), base), math.log(error[2].max(), base), levels, base=base)
 
     contour_z1 = ax0.contourf(x_grid, y_grid, error[0], levels=levels, cmap='jet')
     ax0.set_title(f"G_1")
@@ -77,8 +82,44 @@ def plot_heat_map_loss(x_grid, y_grid, target, pred):
     ax2.set_xlabel("log(R)")
     ax2.set_ylabel("log(S)")
 
-    fig.colorbar(contour_z1)
-    fig.colorbar(contour_z2)
-    fig.colorbar(contour_z3)
+    tick = [0.0, 0.0001, 0.002, 0.01, 0.07]
+
+    cbar_1 = fig.colorbar(contour_z1, format="%f")
+
+    cbar_2 = fig.colorbar(contour_z2)
+
+    cbar_3 = fig.colorbar(contour_z3)
+
 
     plt.show()
+
+def plot_heat_map_3D(x_grid, y_grid, z_grid):
+    fig, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(20, 5))
+    levels = 100
+
+    contour_z1 = ax0.contourf(x_grid, y_grid, z_grid[0], levels=levels, cmap='jet')
+    ax0.set_title(f"G_1")
+    ax0.set_xlabel("log(R)")
+    ax0.set_ylabel("log(S)")
+
+    contour_z2 = ax1.contourf(x_grid, y_grid, z_grid[1], levels=levels, cmap='jet')
+    ax1.set_title(f"G_2")
+    ax1.set_xlabel("log(R)")
+    ax1.set_ylabel("log(S)")
+
+    contour_z3 = ax2.contourf(x_grid, y_grid, z_grid[2], levels=levels, cmap='jet')
+    ax2.set_title(f"G_3")
+    ax2.set_xlabel("log(R)")
+    ax2.set_ylabel("log(S)")
+
+    tick = [0.0, 0.0001, 0.002, 0.01, 0.07]
+
+    cbar_1 = fig.colorbar(contour_z1, format="%.4f")
+    cbar_1.minorticks_off()
+
+    cbar_2 = fig.colorbar(contour_z2, format="%.4f")
+
+    cbar_3 = fig.colorbar(contour_z3, format="%.4f")
+
+    plt.show()
+
