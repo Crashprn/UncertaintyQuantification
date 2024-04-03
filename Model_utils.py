cbb97007978903e4d38070b1dd838341fc0899d0
Cody Grogan <thewall907@gmail.com>
2024-04-03 14:48:02 -0600

    Added super computer main file

--- Model_utils.py
+++ Model_utils.py
@@ -1,4 +1,6 @@
 import os
+import dill
+import json
 
 def create_skorch_model(model, skorch_NN_type, model_args, skorch_args):
     initialized_model = model(**model_args)
@@ -15,3 +17,19 @@ def reinitialize_model(pt_name, checkpoint_dir, model, model_type, net_params, t
     )
     return net
 
+
+def save_MCMC_model(mcmc_obj, save_dir, save_name):
+    mcmc_obj.sampler = None
+
+    with open(os.path.join(save_dir, save_name), 'wb') as f:
+        dill.dump(mcmc_obj, f)
+
+def load_MCMC_model(save_dir, save_name):
+    with open(os.path.join(save_dir, save_name), 'rb') as f:
+        mcmc_obj = dill.load(f)
+    return mcmc_obj
+
+def get_params_from_json(json_file):
+    with open(json_file, 'r') as f:
+        params = json.load(f)
+    return params
