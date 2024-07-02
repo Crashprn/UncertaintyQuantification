ae26557c2d193e97f704c151d75befed87d9eef3
Cody Grogan <thewall907@gmail.com>
2024-07-02 11:56:04 -0600

    Edits

--- run_gp_script.sh
+++ run_gp_script.sh
@@ -19,6 +19,7 @@ cat > $1.batch << EOF
 #SBATCH --time=00-03:00:00
 #SBATCH --nodes=$NN
 #SBATCH --ntasks=$NP
+#SBATCH -p notchpeak
 #SBATCH -o out.$NN
 #SBATCH -e err.$NN
 #SBATCH --account=usumae-np
