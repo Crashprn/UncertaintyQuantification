837a65fa3734008afbf6ccdc7f45eaca6cbbae28
Cody Grogan <thewall907@gmail.com>
2024-07-30 11:26:44 -0600

    Fixed run script bugs

--- run_hmc_script.sh
+++ run_hmc_script.sh
@@ -2,22 +2,19 @@
 
 #PBS -A BuoyDrivenFlows
 #PBS -l walltime=24:00:00
-#PBS -l select=1:system=polaris
+#PBS -l select=10:ncpus=32:ngpus=4:system=polaris
 #PBS -l filesystems=home:grand
-#PBS -q debug
-#PBS -o out.$PBS_JOBID
-#PBS -e err.$PBS_JOBID
+#PBS -q prod
+#PBS -o out.txt
+#PBS -e err.txt
 
 
 module use /soft/modulefiles
 module load conda
 conda activate base
-cat ${PBS_O_WORKDIR}
 cd ${PBS_O_WORKDIR}
-source /venv/2024-04-29/bin/activate
+source ./venvs/2024-04-29/bin/activate
 sleep 1
-python3 polaris_hmc_main.py
+python3 -u polaris_hmc_main.py
 sleep 1
 exit 0
-EOF
-
