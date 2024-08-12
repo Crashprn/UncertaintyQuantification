ad7d69c5e9f0fe4e1050f65123070f8054b84dfe
Cody Grogan <thewall907@gmail.com>
2024-08-12 13:16:55 -0600

    Saving hmc state after each block for continuous run

--- polaris_scripts/polaris_hmc_warmup.py
+++ polaris_scripts/polaris_hmc_warmup.py
@@ -149,10 +149,12 @@ def run_warmup(parser, model, init_strat, hmc_params, mcmc_params, warmup_iters,
         )
 
         current_iter += num_iters
+        hmc_state = hmc_states[1]
 
         if parser.verbose:
             print(f"Saving Warmup Checkpoint after {current_iter} of {total_iterations} iterations to {save_dir} as {save_prefix}_warm_state.pkl")
         pickle.dump(hmc_states[1], open(os.path.join(save_dir, f"{save_prefix}_warm_state.pkl"), "wb"))
+
 
     if parser.verbose:
         if current_iter >= total_iterations:
