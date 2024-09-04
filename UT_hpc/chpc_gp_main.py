f2976802655192ac69c477ceb1bf2ae25094e70f
Cody Grogan <thewall907@gmail.com>
2024-09-04 12:35:45 -0600

    Added float type and added test error output

--- UT_hpc/chpc_gp_main.py
+++ UT_hpc/chpc_gp_main.py
@@ -20,6 +20,7 @@ from src.utils.data_utils import *
 DATA_BOUNDS_LOG = (-.5, 2)
 #DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 DEVICE = torch.device('cpu')
+DTYPE = torch.float64
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -29,11 +30,11 @@ def parse_args():
     )
 
     parser.add_argument('--save_dir', '-d', type=str, default='data/GP')
-    parser.add_argument('--n_data', type=int, default=2_000)
+    parser.add_argument('--n_data', type=int, default=1_000)
     parser.add_argument('--n_restarts', type=int, default=0)
     parser.add_argument('--grid_dim', type=int, default=700)
     parser.add_argument('--verbose', '-v', type=int, default=1)
-    parser.add_argument('--batch_size', type=int, default=4000)
+    parser.add_argument('--batch_size', type=int, default=1000)
     parser.add_argument('--dim_y', type=int, default=0)
     parser.add_argument('--run_name', type=str, default='GP')
 
@@ -54,8 +55,8 @@ def train_test(parser):
     x_train = x_scaler.transform(etas_train).astype(np.float32)
     y_train = y_scaler.transform(gs_train).astype(np.float32)
 
-    x_train = torch.tensor(x_train, device=DEVICE)
-    y_train = torch.tensor(y_train, device=DEVICE)
+    x_train = torch.tensor(x_train, device=DEVICE, dtype=DTYPE)
+    y_train = torch.tensor(y_train, device=DEVICE, dtype=DTYPE)
 
 
     kernel = RBFKernel(amplitude=1.0, length_scale=1.0, device=DEVICE).to(DEVICE)
@@ -65,8 +66,9 @@ def train_test(parser):
         batch_size=parser.batch_size,
         device=DEVICE,
         verbose=parser.verbose,
-        max_iter=1000,
-        lr=0.001
+        max_iter=2000,
+        lr=1e-3,
+        delta=1e-6
     )
 
     if parser.verbose:
@@ -99,7 +101,7 @@ def train_test(parser):
     num_splits = 100
 
     for x_split in np.array_split(etas_test, num_splits):
-        x_split = torch.tensor(x_split, device=DEVICE, dtype=torch.float32)
+        x_split = torch.tensor(x_split, device=DEVICE, dtype=DTYPE)
         mean, std = gp.predict(x_split, return_std=True)
         pred_mean.append(y_scaler.inverse_transform(mean.reshape(-1, 1).detach().cpu().numpy()))
         pred_std.append(std.detach().cpu().numpy() * y_scaler.scale_)
@@ -110,6 +112,8 @@ def train_test(parser):
     if parser.verbose:
         print("---> Predictions Complete")
         print("---> Saving Predictions to ", save_dir)
+        target = gs_test[:, parser.dim_y]
+        print("---> Test Error: ", np.mean(np.abs(target - pred_mean.squeeze())))
 
     np.save(os.path.join(save_dir, f"{parser.run_name}_Mean{parser.dim_y}.npy"), pred_mean)
     np.save(os.path.join(save_dir, f"{parser.run_name}_Std{parser.dim_y}.npy"), pred_std)
